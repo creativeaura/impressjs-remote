34f956a44a4754a9c2d84024d2acb0081eef0b78
Gaurav Jassal <gauravjassal@gmail.com>
2012-06-22 23:12:20 +0100

    Styles added for mobile web remote

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'ImpressJS Remote' });
+  res.render('index', { title: 'ImpressJs' });
 };
 
 exports.session = function(req, res){
